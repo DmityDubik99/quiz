a833d339fae5e75326d8ded966eff1ea4a7d29ee
Dmitry Dubovicky <duboviczkij_dmitrij@mail.ru>
2025-04-15 15:35:00 +0300

    dima_updateMenu - update connector

--- games/conector.py
+++ games/conector.py
@@ -1,11 +1,20 @@
 from games.fill_in_the_blank import FillInBlanck
+import random
 
 class Conector:
     def __init__(self, file_path: str):
         print("🟢 Инициализация Conector")
         self.quiz_loader = FillInBlanck(file_path)
 
-    def connect_to_fill_in_blank(self, level=None, topic=None, subtopic=None):
+    def quizFiilTheBlank(self):
+        connector = Conector("bd/fill_in_the_blank/english_quiz_a1.json")
+        connector.connect_to_fill_in_blank(topic="Travel", subtopic="Directions")
+
+
+    def connect_to_fill_in_blank(self, level="rnd", topic=None, subtopic=None):
+        if level == "rnd":
+            level = random.choice(["A1", "A2", "B1", "B2", "C1", "C2"])
+
         quiz = self.quiz_loader.get_quiz(topic=topic, subtopic=subtopic)
 
         if quiz:
